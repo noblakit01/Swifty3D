fa82b459b13ba71767d97b599ca82ce0e6c8f44c
Luan Tran <noblakit01@gmail.com>
2017-08-18 15:23:04 +0700

    Add extension float help convert degree to radian

---
+++ Sources/FloatPointExt.swift
@@ -0,0 +1,14 @@
+//
+//  FloatPointExt.swift
+//  Swifty3dCell
+//
+//  Created by Minh Luan Tran on 8/18/17.
+//
+//
+
+import Foundation
+
+
+extension FloatingPoint {
+    var degreesToRadians: Self { return self * .pi / 180 }
+}
