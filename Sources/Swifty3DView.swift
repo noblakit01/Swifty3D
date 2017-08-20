bf335c4857e2a6e87758ceacc54c4c89d40adf2d
noblakit01 <noblakit01@gmail.com>
2017-08-20 21:11:47 +0700

    hide shadow opacity

--- Sources/Swifty3DView.swift
+++ Sources/Swifty3DView.swift
@@ -15,6 +15,9 @@ public class Swifty3DView: UIView {
         super.touchesBegan(touches, with: event)
         
         startMoving()
+        if let location = touches.first?.location(in: self) {
+            moving(location)
+        }
     }
     
     override public func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -131,6 +134,14 @@ public class Swifty3DView: UIView {
         shadowOffsetAnimation.isRemovedOnCompletion = false
         shadowOffsetAnimation.timingFunction = CAMediaTimingFunction(name: "easeOut")
         layer.add(shadowOffsetAnimation, forKey: "shadowOffset")
+        
+        let shadowOpacityAnimation = CABasicAnimation(keyPath: "shadowOpacity")
+        shadowOpacityAnimation.toValue = 0.0
+        shadowOpacityAnimation.duration = animationDuration
+        shadowOpacityAnimation.isRemovedOnCompletion = false
+        shadowOpacityAnimation.fillMode = kCAFillModeForwards
+        shadowOpacityAnimation.timingFunction = CAMediaTimingFunction(name: "easeOut")
+        layer.add(shadowOpacityAnimation, forKey: "shadowOpacityAnimation")
         
         let scaleAnimation = CABasicAnimation(keyPath: "transform")
         scaleAnimation.toValue = NSValue(caTransform3D: targetScaleTransform)
