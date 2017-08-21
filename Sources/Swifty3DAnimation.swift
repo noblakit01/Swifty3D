fe5e61e092ff18eb9cb7da49e2b06918a9196bc8
noblakit01 <noblakit01@gmail.com>
2017-08-21 21:31:32 +0700

    Create Swifty3DAnimation protocol. And add some properties.

---
+++ Sources/Swifty3DAnimation.swift
@@ -0,0 +1,63 @@
+//
+//  Swifty3DAnimation.swift
+//  Swifty3dCell
+//
+//  Created by luan on 8/21/17.
+//
+//
+
+import UIKit
+
+protocol Swifty3DAnimation: class {
+    var shadowFactor: CGFloat {get}
+    
+    var animationDuration: CFTimeInterval {get}
+    
+    var highlightedScale: CGFloat {get}
+    
+    var rotateXFactor: CGFloat {get}
+    
+    var rotateYFactor: CGFloat {get}
+    
+    var rotateZFactor: CGFloat {get}
+    
+    var maxTranslationX: CGFloat {get}
+    
+    var maxTranslationY: CGFloat {get}
+}
+
+extension Swifty3DAnimation {
+    var shadowFactor: CGFloat {
+        get {
+            return 10.0
+        }
+    }
+    
+    var animationDuration: CFTimeInterval {
+        return 0.4
+    }
+    
+    var highlightedScale: CGFloat {
+        return 1.22
+    }
+    
+    var rotateXFactor: CGFloat {
+        return 12
+    }
+    
+    var rotateYFactor: CGFloat {
+        return 14
+    }
+    
+    var rotateZFactor: CGFloat {
+        return 9
+    }
+    
+    var maxTranslationX: CGFloat {
+        return 3.5
+    }
+    
+    var maxTranslationY: CGFloat {
+        return 2.5
+    }
+}
