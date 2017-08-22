a2a8250441e1f084cb64a3196f13e02b9b25a12b
noblakit01 <noblakit01@gmail.com>
2017-08-22 22:24:56 +0700

    Add Swifty3D Collection View Cell

---
+++ Sources/Swifty3DCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  Swifty3DCollectionViewCell.swift
+//  Swifty3dCell
+//
+//  Created by luan on 8/22/17.
+//
+//
+
+import UIKit
+
+class Swifty3DCollectionViewCell: UICollectionViewCell {
+    override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        startMoving()
+        if let location = touches.first?.location(in: self) {
+            moving(location)
+        }
+    }
+    
+    override public func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        
+        if let location = touches.first?.location(in: self) {
+            moving(location)
+        }
+    }
+    
+    override public func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        
+        stopMoving()
+    }
+    
+    override public func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        
+        stopMoving()
+    }
+}
