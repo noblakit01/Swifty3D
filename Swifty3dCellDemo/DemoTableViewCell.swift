ee9225279b5dee7e665dfb01ffb328782d957416
Luan Tran <noblakit01@gmail.com>
2017-08-24 16:03:07 +0700

    Add demo project with tableview cell

---
+++ Swifty3dCellDemo/DemoTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  DemoTableViewCell.swift
+//  Swifty3dCell
+//
+//  Created by Minh Luan Tran on 8/24/17.
+//
+//
+
+import UIKit
+import Swifty3dCell
+
+class DemoTableViewCell: Swifty3DTableViewCell {
+
+    @IBOutlet weak var contentImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Swifty3dCellDemo/DemoTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DemoTableViewController.swift
+//  Swifty3dCell
+//
+//  Created by Minh Luan Tran on 8/24/17.
+//
+//
+
+import UIKit
+
+class DemoTableViewController: UIViewController {
+
+    var imageNames = [
+        "dog-1",
+        "dog-2",
+        "dog-3",
+        "dog-4",
+        "dog-5",
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension DemoTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! DemoTableViewCell
+        cell.contentImageView.image = UIImage(named: imageNames[indexPath.row])
+        return cell
+    }
+}
